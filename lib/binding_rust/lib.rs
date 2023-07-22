c93c96bb194997c99825a288c119df2ea952ad26
Andrew Hlynskyi <ahlincq@gmail.com>
2023-07-23 02:58:56 +0300

    More general TextProvider for F

--- lib/binding_rust/lib.rs
+++ lib/binding_rust/lib.rs
@@ -2210,12 +2210,13 @@ impl<'cursor, 'tree> fmt::Debug for QueryMatch<'cursor, 'tree> {
     }
 }
 
-impl<'a, F, I> TextProvider<&'a [u8]> for F
+impl<'a, F, R, I> TextProvider<I> for F
 where
-    F: FnMut(Node) -> I,
-    I: Iterator<Item = &'a [u8]>,
+    F: FnMut(Node) -> R,
+    R: Iterator<Item = I>,
+    I: AsRef<[u8]>,
 {
-    type I = I;
+    type I = R;
 
     fn text(&mut self, node: Node) -> Self::I {
         (self)(node)
